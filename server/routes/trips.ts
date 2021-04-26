f52e8dba14eaa93d37435b4dfc9ce3b2d472f301
Manuel <manuel.karner@edu.fh-joanneum.at>
2021-04-26 15:16:10 +0200

    Added Swagger for Trips

--- server/routes/trips.ts
+++ server/routes/trips.ts
@@ -107,11 +107,140 @@ module.exports = function(router, jwtAuth, isOwner, isAdminOrOwner) {
    */
   router.route('/trips/count').get(jwtAuth, tripCtrl.count); //4
 
-
+  /**
+   * @swagger
+   * /trips/{tripId}/{poiId}:
+   *   post:
+   *     summary: Add a POI to a trip.
+   *     description:  Add a POI to a trip.
+   *     tags:
+   *       - Trips
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/NewTrip'
+   *     security:
+   *       - jwt: []
+   *     responses:
+   *       200:
+   *         description: The added POI.
+   *         content:
+   *           application/json:
+   *             schema:
+   *                 $ref: '#/components/schemas/DBTrip'
+   *       401:
+   *         description: Permission insufficient
+   *       403:
+   *         description: Permission insufficient
+   */
   router.route('/trips/:tripId/addPOI').post(jwtAuth, isOwner, poiCtrl.setCreatorAndLocType, poiCtrl.insert, tripCtrl.addPoi, tripCtrl.show); //5 //TODO check
+
+  /**
+   * @swagger
+   * /trips/{tripId}:
+   *   put:
+   *     summary: Update a trip
+   *     description: Updates a trip with the given trip id
+   *     tags:
+   *       - Trips
+   *     security:
+   *       - jwt: []
+   *     responses:
+   *       200:
+   *         description: Successfully updated the following trip
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               items:
+   *                 $ref: '#/components/schemas/DBTrip'
+   *       401:
+   *         description: Permission insufficient
+   *       403:
+   *         description: Permission insufficient
+   *       500:
+   *         description: Trip not found
+   */
   router.route('/trips/:tripId').put(jwtAuth, isOwner, tripCtrl.setCreator, tripCtrl.update, tripCtrl.show); //6
+
+  /**
+   * @swagger
+   * /trips/{tripId}:
+   *   get:
+   *     summary: Get a trip
+   *     description: Retrieve trip from TravelLog by the given id.
+   *     tags:
+   *       - Trips
+   *     security:
+   *       - jwt: []
+   *     responses:
+   *       200:
+   *         description: The Trip.
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               items:
+   *                 $ref: '#/components/schemas/DBTrip'
+   *       401:
+   *         description: Permission insufficient
+   *       403:
+   *         description: Permission insufficient
+   *       500:
+   *         description: Trip not found
+   */
   router.route('/trips/:tripId').get(jwtAuth, tripCtrl.show); //7 //LoadingTrips
+
+  /**
+   * @swagger
+   * /trips/{tripId}/{poiId}:
+   *   delete:
+   *     summary: Delete a poi from a trip
+   *     description: Delete a poi with the given id from a trip in the TravelLog.
+   *     tags:
+   *       - Trips
+   *     security:
+   *       - jwt: []
+   *     responses:
+   *       200:
+   *         description: Poi deleted from trip
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               items:
+   *                 $ref: '#/components/schemas/DBTrip'
+   *       401:
+   *         description: Permission insufficient
+   *       403:
+   *         description: Permission insufficient
+   *       500:
+   *         description: Poi not found
+   */
   router.route('/trips/:tripId/:poiId').delete(jwtAuth, isOwner, tripCtrl.removePoi, tripCtrl.show); //8 //TODO
+
+  /**
+   * @swagger
+   * /trips/{tripId}:
+   *   delete:
+   *     summary: Delete a trip by id
+   *     description: Delete a trip from TravelLog.
+   *     tags:
+   *       - Trips
+   *     security:
+   *       - jwt: []
+   *     responses:
+   *       200:
+   *         description: Trip deleted
+   *       401:
+   *         description: Permission insufficient
+   *       403:
+   *         description: Permission insufficient
+   *       500:
+   *         description: Trip not found
+   */
   router.route('/trips/:tripId').delete(jwtAuth, isAdminOrOwner, poiCtrl.deleteFromTrip, tripCtrl.delete); //9
 
 
